415de3f83d1e13873b770b0a1308180cea474fa5
chriscant <chris@phdcc.com>
2020-05-24 12:42:18 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,129 @@
+# mkdistmaps
+
+Command line tool to make species distribution maps on top of a base layer image from 
+[GB](https://en.wikipedia.org/wiki/Ordnance_Survey_National_Grid)
+species records with grid references.
+[Irish](https://en.wikipedia.org/wiki/Irish_grid_reference_system) grid references not supported yet.
+
+# Usage
+
+Ensure you have [nodejs](https://nodejs.org/en/download/) installed.
+You will also need your records in CSV format and a base map image.
+
+At a suitable command line prompt:
+```
+git clone https://github.com/chriscant/mkdistmaps.git
+cd mkdistmaps
+npm install
+
+```
+
+* Now get your CSV of records, basemap image and a font file into the new mkdistmaps directory
+* Make a text file to configure the run eg [sample-config.json](sample-config.json) which you can rename to have a txt extension if that's easier eg vc101-config.txt
+* Edit your config file to specify your CSV file(s), a font, an output directory and other options
+* Now do this at the command line eg:
+
+```
+node index.js vc101-config.txt
+```
+
+The code reads all the records in the given CSV file(s) and then creates one map for each found species in the output folder.
+A monad or hectad is coloured in if there are records for that species in that square.
+The fill colour is determined by the date of the most recent record for that square.
+
+If you option to make monad maps, then:
+* the monad is shown as a circle if there is only one record for the monad
+* any records that are only at hectad level are shown as outline squares, rather than filled squares
+
+example-hectad.png
+example-monad.png
+
+The code generates basic output as it processes the data, finishing like this:
+
+```...
+Parsed 56980 rows
+Errors 1
+#0 LC0003050000XIWC Date invalid:undefined Year:NaN
+Species: 1165
+Records: 56980
+Empty rows: 0
+Boxes: 949
+Runtime: 189 seconds```
+
+## Configuration
+
+What the program run is determined by a configuration file.
+The config file must be in [JavaScript Object Notation (JSON) format](https://www.w3schools.com/js/js_json.asp).
+The sample config file shows some of the options available.
+
+### outputFolder
+
+* Specify the output folder, which is created if need be
+
+### recordset
+
+* **csv** path to csv, optionally including wildcard asterisks
+* **title** text that is added to every generated map
+* **GRCol** name of the CSV column that contains the grid reference
+* **TaxonCol** name of the CSV column that contains the species name
+* **ObsKeyCol** optional name of the CSV column containing an observation key - used to show errors
+* **DateCol** specify either this or **YearCol** as the name of the CSV column containing the date
+* **YearCol** name of the CSV column containing the year
+* **DateFormats** optional date formats tried. The defaul is `[ "DD/MM/YYYY", "YYYY" ]` - [see here](https://momentjs.com/docs/#/parsing/string-formats/)
+* **headers** optional array of strings containing the CSV column names
+* **renameHeaders** optional specify true to replace the CSV header line with that in **headers**. The default is `false`
+
+If your CSV file doesn't contains a header line, then supply the column names in **headers**.
+
+If your CSV files does contain a header line but it contains duplicate column names, then you can replace it with the 
+column names given in **headers** and set **renameHeaders** to `true`.  Here is an example for **headers**:
+
+`[ "Location", "Grid ref", "VC", "checked?", "Recorders", "Date(s)", "Altitude", "Site and visit comments", "BLS no.", "Species", "BLS no.2", "Taxon name", "Group", "Status", "Substrate", "Small scale habitats", "Abundance", "Record notes", "Herbarium", "Specimen", "Determiner", "Photo", "Ccref", "SpecimenCC" ]`
+
+Note: the CSV file has columns `Eastings` and `Northings` then these are cross-checked against the grid reference.
+
+### useMonadsNoHectads
+
+* Optionally specify `true` to map records to monads. Default is `false` for hectads.
+
+### font_filename
+
+* Specify the filename of a font eg TTF
+
+### basemap
+
+The base map would typically be generated in QGIS using "Export as image", noting the N/W/E/S values
+
+* **file** path to the base map
+* **north** north bound of supplied base map
+* **west** west bound of supplied base map
+* **east** east bound of supplied base map
+* **south**  south bound of supplied base map
+* **scaledown** scale reduction factor for output 
+* **showhectadname** set to `true` to show the hectad name on each map
+
+### datecolours
+
+* Optionally, specify the date range colours. The default is:
+
+```[
+  { "minyear": 0, "maxyear": 1959, "colour": "rgba(255,255,0, 1)" },  // Yellow
+  { "minyear": 1960, "maxyear": 1999, "colour": "rgba(0,0,255, 1)" }, // Blue
+  { "minyear": 2000, "maxyear": 2019, "colour": "rgba(255,0,0, 1)" }, // Red
+  { "minyear": 2020, "maxyear": 2039, "colour": "rgba(0,255,0, 1)" }  // Green
+]```
+
+
+## Used with thanks
+
+* Josh Marinacci's [pureimage](https://www.npmjs.com/package/pureimage) - [docs](http://joshmarinacci.github.io/node-pureimage/) and contributors
+* C2FO's  [fast-csv](https://www.npmjs.com/package/fast-csv) - [docs](https://c2fo.io/fast-csv/docs/introduction/getting-started) and contributors
+* Other nodejs modules - see code
+
+I built base maps using [QGIS](https://qgis.org/en/site/) and [Ordnance Survey open data](https://www.ordnancesurvey.co.uk/opendatadownload/products.html)
+
+I used data from the [British Lichen Society](https://www.britishlichensociety.org.uk/)
+
+# License
+
+This code is licensed under the GPL v2.
---
+++ index.js
@@ -0,0 +1,444 @@
+#!/usr/bin/env node
+
+// mkdistmaps
+
+const dt_start = new Date()
+
+const fs = require('fs')
+const glob = require("glob")
+const path = require('path')
+const csv = require('fast-csv')
+const PImage = require('pureimage')
+const execSync = require('child_process').execSync
+const moment = require('moment')
+
+const UKletters1 = [
+  { l: 'S', e: 0, n: 0 },
+  { l: 'N', e: 0, n: 500 },
+  { l: 'H', e: 0, n: 1000 },
+  { l: 'T', e: 500, n: 0 }
+]
+const UKletters2 = [
+  { l: 'A', e: 0, n: 400 },
+  { l: 'B', e: 100, n: 400 },
+  { l: 'C', e: 200, n: 400 },
+  { l: 'D', e: 300, n: 400 },
+  { l: 'E', e: 400, n: 400 },
+  { l: 'F', e: 0, n: 300 },
+  { l: 'G', e: 100, n: 300 },
+  { l: 'H', e: 200, n: 300 },
+  { l: 'J', e: 300, n: 300 },
+  { l: 'K', e: 400, n: 300 },
+  { l: 'L', e: 0, n: 200 },
+  { l: 'M', e: 100, n: 200 },
+  { l: 'N', e: 200, n: 200 },
+  { l: 'O', e: 300, n: 200 },
+  { l: 'P', e: 400, n: 200 },
+  { l: 'Q', e: 0, n: 100 },
+  { l: 'R', e: 100, n: 100 },
+  { l: 'S', e: 200, n: 100 },
+  { l: 'T', e: 300, n: 100 },
+  { l: 'U', e: 400, n: 100 },
+  { l: 'V', e: 0, n: 0 },
+  { l: 'W', e: 100, n: 0 },
+  { l: 'X', e: 200, n: 0 },
+  { l: 'Y', e: 300, n: 0 },
+  { l: 'Z', e: 400, n: 0 }
+]
+
+// Get version from last git commit
+const version = "mkdistmaps " + execSync('git describe --tags --long')
+console.log(version)
+
+// Display usage
+if (process.argv.length <= 2) {
+  console.log("usage: node index.js <config.json>")
+  return
+}
+
+// Load config file and remove UTF-8 BOF and any comments starting with //
+let configtext = fs.readFileSync(path.resolve(__dirname, process.argv[2]), { encoding: 'utf8' })
+if (configtext.charCodeAt(0) === 65279) { // Remove UTF-8 start character
+  configtext = configtext.slice(1)
+}
+while (true) {
+  const dslashpos = configtext.indexOf('//')
+  if (dslashpos === -1) break
+  const endlinepos = configtext.indexOf("\n", dslashpos)
+  if (endlinepos === -1) {
+    configtext = configtext.substring(0, dslashpos)
+    break
+  }
+  configtext = configtext.substring(0, dslashpos) + configtext.substring(endlinepos)
+}
+const config = JSON.parse(configtext)
+console.log(config)
+
+// Make output folder if need be
+fs.mkdirSync(path.join(__dirname, config.outputFolder), { recursive: true })
+
+// Set scale factor for hectad or monad
+const hectadSCALE = {
+  factor: 10000,
+  factor2: 10000,
+  gridreffigs:2
+}
+const monadSCALE = {
+  factor: 1000,
+  factor2: 10000,
+  gridreffigs:4
+}
+if (!config.hasOwnProperty('useMonadsNoHectads')) {
+  console.log('Using default: map to hectad')
+  config.useMonadsNoHectads = false
+}
+const SCALE = config.useMonadsNoHectads ? monadSCALE : hectadSCALE
+
+// Set default datecolours if need be
+if (!config.hasOwnProperty('datecolours')) {
+  console.log("Using default datecolours")
+  config.datecolours = [
+    { "minyear": 0, "maxyear": 1959, "colour": "rgba(255,255,0, 1)" },  // Yellow
+    { "minyear": 1960, "maxyear": 1999, "colour": "rgba(0,0,255, 1)" }, // Blue
+    { "minyear": 2000, "maxyear": 2019, "colour": "rgba(255,0,0, 1)" }, // Red
+    { "minyear": 2020, "maxyear": 2039, "colour": "rgba(0,255,0, 1)" }  // Green
+  ]
+}
+
+// Set default DateFormats if need be
+if (!config.recordset.DateFormats) {
+  console.log("Using default DateFormats")
+  config.recordset.DateFormats = ["DD/MM/YYYY", "YYYY"];
+}
+
+/////////////////
+// Do everything!
+const headers = config.recordset.headers ? config.recordset.headers: true
+const renameHeaders = config.recordset.renameHeaders ? config.recordset.renameHeaders : false
+
+let totalrecords = 0
+let anyincsv = 0;
+async function processFiles() {
+  const files = glob.sync(config.recordset.csv)
+  if (files.length === 0) {
+    console.error('NO FILE(S) FOUND FOR: ', config.recordset.csv)
+  }
+  let donecount = 0
+  const doFiles = new Promise((resolve, reject) => {
+    for (const file of Object.values(files)) {
+      //console.log(file)
+      anyincsv = 0
+      fs.createReadStream(path.resolve(__dirname, file))
+        .pipe(csv.parse({ headers: headers, renameHeaders: renameHeaders }))
+        .on('error', error => console.error(error))
+        .on('data', row => { processLine(file, row) })
+        .on('end', function (rowCount) {
+          console.error(file, 'Species:', Object.keys(speciesesGrids).length, anyincsv === 0 ? 'EMPTY' :'')
+          totalrecords += rowCount
+          if (++donecount === files.length) {
+            resolve()
+          }
+        })
+    }
+  })
+  await doFiles
+  console.log("COMPLETED READING DATA")
+  importComplete(totalrecords)
+}
+processFiles()
+// ASYNC: DO NOT CODE HERE
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+// processLine: Process a line of CSV data ie a single record
+
+// 'Spatial Reference': 'NY30',     Eastings: '330001', Northings: '500000',
+// 'Spatial Reference': 'NY3703',   Eastings: '337001', Northings: '503000',
+// 'Spatial Reference': 'NY387034', Eastings: '338701', Northings: '503400',
+//                       NY48311327            348311               513270
+//                       NY50951510                                           Some 12 figure GRs appear as 10 figures
+//                       NY7432046814
+
+const speciesesGrids = {}
+const errors = []
+let lineno = 0
+let records = 0
+let empties = 0
+const boxes = {}
+
+function processLine(file, row) {
+  //console.log(row)
+  lineno++
+
+  // Get GR and species name
+  const SpatialReference = row[config.recordset.GRCol].toUpperCase()
+  //console.log('SpatialReference', SpatialReference)
+  const TaxonName = row[config.recordset.TaxonCol]
+  if (SpatialReference.length === 0 || TaxonName.length === 0 || TaxonName.substring(0,1)==='#') {
+    empties++
+    return
+  }
+
+  records++
+  anyincsv++
+
+  // Get other data fields
+  const EastingsExplicit = parseInt(row['Eastings'])
+  const NorthingsExplicit = parseInt(row['Northings'])
+  const ExplicitGiven = EastingsExplicit !== 0 || NorthingsExplicit!==0
+  let ObsKey = row[config.recordset.ObsKeyCol]
+  let ObsDate = row[config.recordset.DateCol]
+  let Year = parseInt(row[config.recordset.YearCol])
+  //console.log('----------')
+  //console.log('SpatialReference', SpatialReference)
+  //console.log('EastingsExplicit', EastingsExplicit)
+  //console.log('NorthingsExplicit', NorthingsExplicit)
+  //console.log('TaxonName', TaxonName, TaxonName.length)
+
+  if (!ObsKey) ObsKey = "Line#" + lineno
+
+  // Decode Date or Year
+  let YearFound = Year
+  if (!config.recordset.YearCol) {
+    const ObsDate2 = moment(ObsDate, config.recordset.DateFormats)
+    if (ObsDate2.isValid()) {
+      YearFound = true
+      Year = ObsDate2.year()
+    }
+  }
+  if (!YearFound) {
+    errors.push(ObsKey + " Date invalid:" + ObsDate + " Year:" + Year)
+    return
+  }
+
+  // From grid reference, work out Eastings and Northings and box name eg NY51 or NY5714
+  let Eastings = 0;
+  let Northings = 0;
+
+  let box = SpatialReference
+  const grfig = SCALE.gridreffigs / 2
+  if (box.length === 12) {
+    Eastings += parseInt(box.substring(2, 7))
+    Northings += parseInt(box.substring(7))
+    box = box.substring(0, 2 + grfig) + box.substring(7, 7 + grfig)
+  }
+  else if (box.length === 10) {
+    Eastings += parseInt(box.substring(2, 6)) * 10
+    Northings += parseInt(box.substring(6)) * 10
+    box = box.substring(0, 2 + grfig) + box.substring(6, 6 + grfig)
+  }
+  else if (box.length === 8) {
+    Eastings += parseInt(box.substring(2, 5)) * 100
+    Northings += parseInt(box.substring(5)) * 100
+    box = box.substring(0, 2 + grfig) + box.substring(5, 5 + grfig)
+  }
+  else if (box.length === 6) {
+    Eastings += parseInt(box.substring(2, 4)) * 1000
+    Northings += parseInt(box.substring(4)) * 1000
+    box = box.substring(0, 2 + grfig) + box.substring(4, 4 + grfig)
+  }
+  else if (box.length === 4) {
+    Eastings += parseInt(box.substring(2, 3)) * 10000
+    Northings += parseInt(box.substring(3)) * 10000
+  }
+  else {
+    errors.push(ObsKey + " Spatial Reference duff length: " + box.length)
+    return
+  }
+
+  const l1 = box.substring(0, 1)
+  for (let i = 0; i < UKletters1.length; i++){
+    const boxbl = UKletters1[i]
+    if (boxbl.l == l1) {
+      //console.log('boxbl', boxbl)
+      Eastings += boxbl.e * 1000
+      Northings += boxbl.n * 1000
+      break
+    }
+  }
+
+  const l2 = box.substring(1, 2)
+  for (let i = 0; i < UKletters2.length; i++) {
+    const boxbl = UKletters2[i]
+    if (boxbl.l == l2) {
+      //console.log('boxbl', boxbl)
+      Eastings += boxbl.e * 1000
+      Northings += boxbl.n * 1000
+      break
+    }
+  }
+
+  if (ExplicitGiven) {
+    if ((Math.abs(Eastings - EastingsExplicit) > 2) || (Math.abs(Northings - NorthingsExplicit) > 2)) {
+      errors.push(ObsKey + " Explicit grid ref discrepancy: " + SpatialReference + " ExpE:" + EastingsExplicit + " ExpN:" + NorthingsExplicit + " E:" + Eastings + " N:" + Northings)
+      return
+    }
+  }
+  //console.log('box', box)
+  //console.log('Eastings', Eastings)
+  //console.log('Northings', Northings)
+
+  // Save box name and location
+  if (!boxes[box]) {
+    boxes[box] = {
+      e: Math.floor(Eastings / SCALE.factor),
+      n: Math.floor(Northings / SCALE.factor)
+    }
+  }
+
+  // Add/Update record for species ie count, min and max year for each box
+  let speciesGrids = speciesesGrids[TaxonName]
+  if (!speciesGrids) {
+    speciesGrids = {}
+    speciesGrids[box] = { count: 0, minyear: 3000, maxyear: 0 }
+    speciesesGrids[TaxonName] = speciesGrids
+  }
+  if (!speciesGrids[box]) {
+    speciesGrids[box] = { count: 0, minyear: 3000, maxyear: 0 }
+  }
+  speciesGrids[box].count++
+  if (Year > speciesGrids[box].maxyear) {
+    speciesGrids[box].maxyear = Year
+  }
+  if (Year < speciesGrids[box].minyear) {
+    speciesGrids[box].minyear = Year
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+// importComplete:  Having read all records, generate distribution maps for all found species
+
+async function importComplete(rowCount) {
+  // Load base map and work out main box dimensions (assumed to be square)
+  const img = await PImage.decodePNGFromStream(fs.createReadStream(config.basemap.file))
+  const width = img.width / config.basemap.scaledown
+  const height = img.height / config.basemap.scaledown
+  const mapeastings = config.basemap.east - config.basemap.west     // eg 390500-293169
+  const mapnorthings = config.basemap.north - config.basemap.south  // eg 589703-460155
+  console.log(mapeastings, mapnorthings)
+  const boxswidth = mapeastings / SCALE.factor
+  console.log('boxswidth', boxswidth)
+  let boxwidth = width / boxswidth  // boxheight should be the same if map in proportion
+  console.log('boxwidth', boxwidth)
+  const boxswidth2 = mapeastings / SCALE.factor2
+  console.log('boxswidth2', boxswidth2)
+  const boxwidth2 = width / boxswidth2  // boxheight should be the same if map in proportion
+  console.log('boxwidth2', boxwidth2)
+
+  // Now set image x and y for each box
+  for (const [box, boxloc] of Object.entries(boxes)) {
+    const n = (boxloc.n * SCALE.factor) - config.basemap.south
+    const hn = n / SCALE.factor
+    const hnp = hn * boxwidth
+    boxloc.y = height - hnp - boxwidth
+    const e = (boxloc.e * SCALE.factor) - config.basemap.west
+    const he = e / SCALE.factor
+    const hep = he * boxwidth
+    boxloc.x = hep // - boxwidth
+  }
+  if (boxwidth < 2) boxwidth = 2
+  const boxwidthhalf = boxwidth / 2
+  console.log('boxwidthhalf', boxwidthhalf)
+
+  // Load the font
+  const loadFont = new Promise((resolve, reject) => {
+    const fontpath = path.join(__dirname, config.font_filename)
+    const fnt = PImage.registerFont(fontpath, 'TheFont')
+    fnt.load(() => {
+      resolve()
+    })
+  })
+  await loadFont
+
+  // Go through all species
+  let done = 0
+  for (const [TaxonName, speciesGrids] of Object.entries(speciesesGrids)) {
+    //console.log(TaxonName, speciesGrids)
+
+    // Create output canvas from base map
+    const img2 = PImage.make(width, height)
+    const ctx = img2.getContext('2d')
+    ctx.drawImage(img,
+      0, 0, img.width, img.height,  // source dimensions
+      0, 0, width, height           // destination dimensions
+    )
+
+    // Write summary text
+    ctx.fillStyle = '#000000'
+    ctx.font = "24pt 'TheFont'"
+    ctx.fillText(TaxonName, width / 20, height / 20)
+
+    ctx.font = "24pt 'TheFont'"
+    ctx.fillText(config.recordset.title, width / 20, height * 2 / 20)
+
+    // Go through all boxes found for this species
+    let reccount = 0
+    for (const [box, boxdata] of Object.entries(speciesGrids)) {
+
+      reccount += boxdata.count
+      const boxloc = boxes[box]
+
+      // Determine box colour
+      ctx.fillStyle = "rgba(255,20, 147, 1)" // default to pink
+      for (const datecolour of Object.values(config.datecolours)) {
+        if (boxdata.maxyear >= datecolour.minyear && boxdata.maxyear <= datecolour.maxyear) {
+          ctx.fillStyle = datecolour.colour
+        }
+      }
+      ctx.strokeStyle = ctx.fillStyle
+
+      // Draw solid or round box (and optional hectad outline)
+      if (SCALE.gridreffigs === 4 && box.length === 4) {
+        ctx.strokeRect(boxloc.x, boxloc.y, boxwidth2, boxwidth2)
+      } else {
+        if (boxdata.count === 1 && config.useMonadsNoHectads>1) {
+          ctx.beginPath()
+          ctx.arc(boxloc.x + boxwidthhalf, boxloc.y + boxwidthhalf, boxwidthhalf, 0, 2 * Math.PI, false) // Math.PI
+          ctx.closePath()
+          ctx.fill()
+        } else {
+          ctx.fillRect(boxloc.x, boxloc.y, boxwidth, boxwidth)
+        }
+      }
+
+      // Draw hectad name
+      if (config.showhectadname) {
+        ctx.fillStyle = '#000000'
+        ctx.font = "12pt 'TheFont'"
+        ctx.fillText(box, boxloc.x, boxloc.y + boxwidth)
+      }
+    }
+    // Write number of records
+    ctx.fillStyle = '#000000'
+    ctx.font = "24pt 'TheFont'"
+    ctx.fillText("Records: " + reccount, width / 20, height * 3 / 20)
+
+    // Write number of records
+    ctx.fillStyle = '#808080'
+    ctx.font = "12pt 'TheFont'"
+    ctx.fillText(version, width / 20, height / 60)
+
+    // Output the final species map
+    const outpath = path.join(__dirname, config.outputFolder, TaxonName+".png")
+    await PImage.encodePNGToStream(img2, fs.createWriteStream(outpath))
+    console.log("done", TaxonName, reccount)
+    //if( done++>0)
+    //break
+  }
+
+  // Report record count, errors, species and boxes
+  console.log(`Parsed ${rowCount} rows`)
+  console.log(`Errors ${errors.length}`)
+  for (let i = 0; i < errors.length; i++) {
+    console.error('#' + i, errors[i])
+  }
+  console.log('Species:', Object.keys(speciesesGrids).length)
+  console.log('Records:', records)
+  console.log('Empty rows:', empties)
+  console.log('Boxes:', Object.keys(boxes).length)
+
+  const dt_end = new Date()
+  const runtime_seconds = Math.floor((dt_end - dt_start) / (1000))
+  console.log('Runtime:', runtime_seconds, "seconds")
+}
+///////////////////////////////////////////////////////////////////////////////////////
