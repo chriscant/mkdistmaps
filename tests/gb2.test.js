7e3193d219b7ed8af5250d9924fceaae1c3b0263
chriscant <chris@phdcc.com>
2020-06-28 10:18:05 +0100

    Add test for count map; Update other tests

--- tests/gb2.test.js
+++ tests/gb2.test.js
@@ -5,14 +5,14 @@ test('GB GRID REFS #1', async () => {
 
   const spyclog = jest.spyOn(console, 'log').mockImplementation(testhelper.accumulog)
   const spycerror = jest.spyOn(console, 'error').mockImplementation(testhelper.accumulog)
-  const argv = ['node', '.', 'tests/data/gb1-date-test-config.json']
+  const argv = ['node', '.', 'tests/data/gb2-count-test-config.json']
   let rv = await mkdistmaps.run(argv)
   spyclog.mockRestore()
   spycerror.mockRestore()
   console.log('All console output\n', testhelper.accumulogged())
 
   if (rv === 1) {
-    rv = await testhelper.checkFilesEqual('tests/expected/GB1-Species.png', 'tests/output/GB1-Species.png')
+    rv = await testhelper.checkFilesEqual('tests/expected/GB2-Species.png', 'tests/output2/GB1-Species.png')
   }
 
   expect(rv).toBe(1)
