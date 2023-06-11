e9d9ca8b44f393920fe73588b2d653f2a4ea9151
Chris Cant <chris@phdcc.com>
2023-06-11 23:25:13 +0100

    Debug test fail

--- tests/testhelper.js
+++ tests/testhelper.js
@@ -25,6 +25,9 @@ async function checkFilesEqual(path1, path2) {
   })
   if (!await areFilesEqual) {
     console.log('FILES NOT EQUAL', path1, path2)
+    const stats1 = fs.statSync(path1)
+    const stats2 = fs.statSync(path2)
+    console.log('FILE SIZES', stats1.size, stats2.size)
     return 0
   }
   return 1
